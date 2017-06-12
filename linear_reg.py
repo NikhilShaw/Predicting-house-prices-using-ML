a2eebade4236d2b70b5bde88aece60feb8b8184e
Nikhil Shaw <shaw.nikhil05@gmail.com>
2017-06-12 19:39:50 +0530

    Update linear_reg.py

--- linear_reg.py
+++ linear_reg.py
@@ -10,22 +10,29 @@ from sklearn.metrics.regression import mean_squared_error
 input_file = "kc_house_data.csv"
 df = pd.read_csv(input_file)
 #dropping irrelevant features
-df.drop(['id','date'], axis=1)
+df.drop(['date'], axis=1)
 #plotting using seaborn to visualize relationship
 sns.plt.show()
-sns.pairplot(df, x_vars=["sqft_living"], y_vars="price", kind='reg')
+sns.pairplot(df, x_vars=["sqft_living", "floors",'bathrooms', 'bedrooms', 'sqft_lot', 'sqft_above', 'sqft_basement', 'sqft_basement', 'yr_built'], y_vars="price", kind='reg')
 sns.plt.show()
-print df['sqft_living'].shape
-print df['price'].shape
 
-X = df[['sqft_living']]
-Y = df[['price']]
+X = df[['sqft_living','floors','bathrooms', 'bedrooms', 'sqft_lot', 'sqft_above', 'sqft_basement', 'sqft_basement', 'yr_built']].as_matrix()
+Y = df['price'].values
 train_feature, test_feature, train_target, test_target = train_test_split(X, Y, random_state=1)
 #trying to fit model using linear regression
 reg = LinearRegression()
 reg.fit(train_feature, train_target)
 #predicting the test data
-pred = reg.predict(train_feature)
+pred = reg.predict(test_feature)
 ##calculating mean squared error
-print mean_squared_error(train_target, pred)
+msq_error= mean_squared_error(test_target, pred)
+print 'mean square error: ' , msq_error, '\n'
+##printing score
+print 'Regression accuracy score:  ', reg.score(X, Y), '\n'
+print 'Below is actual price vs predicted price', '\n'
+print (pred), '\n'
+print (train_target)
+
+
+
 
